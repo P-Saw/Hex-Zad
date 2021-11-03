7b538a0a6f6267b7d4f61979c826d07f827f6a3e
Piotr Sawicki <psawicki351@gmail.com>
2021-11-03 12:16:18 +0100

    Added missing error message

--- src/components/Inputs/DiameterInput.js
+++ src/components/Inputs/DiameterInput.js
@@ -1,24 +1,28 @@
 import TextField from '@mui/material/TextField';
+import { StyledErrorMessage } from '../Form/Form.style';
 
-const DiameterInput = ({ input }) => {
+const DiameterInput = ({ input, meta }) => {
   return (
-    <TextField
-      fullWidth
-      {...input}
-      label="Diameter"
-      variant="standard"
-      type="number"
-      inputMode="decimal"
-      helperText="Please use comma as separator"
-      InputProps={{
-        inputProps: {
-          max: 50,
-          min: 0,
-          step: 0.1,
-        },
-      }}
-      required
-    />
+    <>
+      <TextField
+        fullWidth
+        {...input}
+        label="Diameter"
+        variant="standard"
+        type="number"
+        inputMode="decimal"
+        helperText="Please use comma as separator"
+        InputProps={{
+          inputProps: {
+            max: 50,
+            min: 0,
+            step: 0.1,
+          },
+        }}
+        required
+      />
+      {meta.touched && meta.error && <StyledErrorMessage>This field is required.</StyledErrorMessage>}
+    </>
   );
 };
 
